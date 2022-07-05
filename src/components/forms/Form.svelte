5e61bc113d1f703a0fac424638bf2e323c6e849e
Tarik <tarikmerrylees@gmail.com>
2022-07-05 18:05:59 +0100

    form as a seperate component

--- src/components/forms/Form.svelte
+++ src/components/forms/Form.svelte
@@ -4,37 +4,55 @@
   import { postDetails } from '$lib/postDetails.js'
   import { transformColumnNameToLabel } from '$lib/transformColumnNameToLabel.js'
 
-  export let details;
-  let columnNames = Object.keys(details);
+  export let details
   export let table_name
   let saveMessage = ""
+  let remoteDetails = getDetails(table_name)
+  let detailsArray = []
 
   onMount(async () => {
-    let remoteDetails = await getDetails(table_name)
-
     if (Object.keys(remoteDetails).length == 0 && eval('localStorage.'+ table_name)) {
       details = JSON.parse(eval('localStorage.' + table_name));
+      console.log(details)
     } else if(Object.keys(remoteDetails).length > 0) {
-      details = remoteDetails;
+      details = await remoteDetails;
+      console.log(remoteDetails)
     }
     else {
-      return
+      details
+    }
+
+    delete details.id
+    delete details.user_id
+    delete details.created_at
+    // turn details into an array of single key value pairs
+    for (const [column_name, value] of Object.entries(details)) {
+      const pair = JSON.parse(`{"${column_name}": "${value}"}`)
+      if(column_name != "user_id" || column_name != "id" || column_name != "created_at" ) detailsArray.push(pair);
     }
   });
-
+  
   let updateForm  = async () => {    
     const message = await postDetails(details)
-    localStorage.personalDetails = JSON.stringify(details)
+    eval('localStorage.'+ table_name + '= JSON.stringify(' + details + ')') 
     saveMessage = message
   }
 </script>
 
-<section class="personalInfoForm_container">
-  <h2 class="personalInfoForm_header">Enter Your Details</h2>
-  <form id="form-personalDetails" class="form-personalDetails">
-    {#each columnNames as columnName}
-    <input type="text" name={columnName} id={columnName} bind:value={details.columnName} placeholder={transformColumnNameToLabel(columnName)}>
-    {/each}
+<section class="form_container">
+  <h2 class="form_header">Enter Your Details</h2>
+  <form id="form-Details" class="form-Details">
+    {#await remoteDetails}
+      <p>...Loading Form</p>
+      {:then}
+        {#each detailsArray as column}
+        <input type="text"
+          name={Object.keys(column)[0]}
+          id={Object.keys(column)[0]}
+          bind:value={details[Object.keys(column)[0]]}
+          placeholder={transformColumnNameToLabel(Object.keys(column)[0])}>
+        {/each}
+    {/await}
     <button type="button" id="update" on:click={updateForm}>Update</button>
   </form>
   
@@ -42,5 +60,5 @@
 </section>
 
 <style lang="scss">
-  @import '../../styles/personalDetailsForm.scss';
+  @import '../../styles/detailsForm.scss';
 </style>
