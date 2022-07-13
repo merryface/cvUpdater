a2f584e0b3edf99013ddf3dadf5bab44ea894ada
Tarik <tarikmerrylees@gmail.com>
2022-07-13 17:01:42 +0100

    restructure

---
+++ src/lib/components/FormsQualifications.svelte
@@ -0,0 +1,11 @@
+<script>
+  import AddPilotQualification from "./forms/ato_details/AddPilotQualification.svelte";
+  import PilotQualifications from "./forms/ato_details/PilotQualifications.svelte";
+
+  const handleClick = () => {
+    AddPilotQualification.onLoad()
+  }
+</script>
+
+<AddPilotQualification />
+<PilotQualifications />
